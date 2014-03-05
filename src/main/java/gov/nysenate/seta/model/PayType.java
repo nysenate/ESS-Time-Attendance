804f3a7831bd31858e2e5ff8eea68fad94ad9117
Ash Islam <ashislam858@gmail.com>
2014-03-05 17:04:26 -0500

    Added more info for PayType

--- src/main/java/gov/nysenate/seta/model/PayType.java
+++ src/main/java/gov/nysenate/seta/model/PayType.java
@@ -2,17 +2,29 @@ package gov.nysenate.seta.model;
 
 public enum PayType
 {
-    RA("Regular Annual"),
-    SA("Special Annual"),
-    TE("Temporary");
+    RA("Regular Annual", 1820, true),
+    SA("Special Annual", 0, true),
+    TE("Temporary", 0, false);
 
     private String desc;
+    private int minHours;
+    private boolean biweekly;
 
-    PayType(String desc) {
+    private PayType(String desc, int minHours, boolean biweekly) {
         this.desc = desc;
+        this.minHours = minHours;
+        this.biweekly = biweekly;
     }
 
     public String getDesc() {
         return desc;
+    }
+
+    public int getMinHours() {
+        return minHours;
+    }
+
+    public boolean isBiweekly() {
+        return biweekly;
     }
 }
