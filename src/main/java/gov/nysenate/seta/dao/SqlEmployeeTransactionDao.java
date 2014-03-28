54e77815d9f34f13bf1c65c22181ad72449dc015
Ash Islam <ashislam858@gmail.com>
2014-03-28 12:27:40 -0400

    Implemented transaction history

--- src/main/java/gov/nysenate/seta/dao/SqlEmployeeTransactionDao.java
+++ src/main/java/gov/nysenate/seta/dao/SqlEmployeeTransactionDao.java
@@ -30,11 +30,11 @@ public class SqlEmployeeTransactionDao extends SqlBaseDao implements EmployeeTra
 
     protected static final String GET_TRANS_HISTORY_SQL =
         "SELECT aud.NUXREFEM, ptx.CDSTATUS, ptx.CDTRANS, ptx.CDTRANSTYP, ptx.NUCHANGE, ptx.DTTXNORIGIN, ptx.DTTXNUPDATE,\n" +
-        "       ptx.DTEFFECT, ${audColumns}\n" +
+        "       ptx.DTEFFECT ${audColumns}\n" +
         "FROM PM21PERAUDIT aud\n" +
         "LEFT JOIN PD21PTXNCODE ptx ON aud.NUCHANGE = ptx.NUCHANGE\n" +
         "LEFT JOIN (SELECT DISTINCT CDTRANS, CDTRANSTYP FROM PL21TRANCODE) code ON ptx.CDTRANS = code.CDTRANS\n" +
-        "WHERE aud.NUXREFEM = :empId AND ptx.CDSTATUS = 'A' AND ptx.DTEFFECT <= :dateEnd\n" +
+        "WHERE aud.NUXREFEM = :empId AND ptx.CDSTATUS = 'A' AND ptx.DTEFFECT BETWEEN :dateStart AND :dateEnd\n" +
         "AND ptx.CDTRANS IN (:transCodes)\n" +
         "ORDER BY ptx.DTEFFECT DESC, ptx.DTTXNORIGIN DESC";
 
@@ -76,29 +76,14 @@ public class SqlEmployeeTransactionDao extends SqlBaseDao implements EmployeeTra
 
     /** {@inheritDoc} */
     @Override
-    public Map<TransactionType, TransactionRecord> getLastTransRecords(int empId, Set<TransactionType> types,
-                                                                       Date start, Date end) {
-        Map<String, String> selectMap = new HashMap<>();
-
-        /** The set of transaction 'codes' to use in the where clause */
-        Set<String> transCodes = new HashSet<>();
-        for (TransactionType type : types) {
-            transCodes.add(type.name());
-        }
-
-        /** The list of column names to be used in the select statement */
-        List<String> auditColList = TransactionType.getAllDbColumnsList();
-        String auditColumns = StringUtils.join(auditColList, ",");
-        selectMap.put("audColumns", (!auditColumns.isEmpty()) ? ("," + auditColumns) : "");
-
+    public Map<TransactionType, TransactionRecord> getLastTransRecords(int empId, Set<TransactionType> types, Date start, Date end) {
         MapSqlParameterSource params = new MapSqlParameterSource();
         params.addValue("empId", empId);
         params.addValue("dateStart", start);
         params.addValue("dateEnd", end);
-        params.addValue("transCodes", transCodes);
+        params.addValue("transCodes", getTransCodesFromSet(types));
 
-        StrSubstitutor strSub = new StrSubstitutor(selectMap);
-        String sql = strSub.replace(GET_LAST_TRANS_REC_SQL);
+        String sql = applyAuditColumnsInSelectSql(GET_LAST_TRANS_REC_SQL, "audColumns", "", types);
         List<TransactionRecord> transRecordList =
                 remoteNamedJdbc.query(sql, params, new TransactionRecordRowMapper("", "", types));
 
@@ -127,6 +112,79 @@ public class SqlEmployeeTransactionDao extends SqlBaseDao implements EmployeeTra
     /** {@inheritDoc} */
     @Override
     public Map<TransactionType, TransactionHistory> getTransHistoryMap(int empId, Set<TransactionType> types, Date start, Date end) {
-        return null;
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("empId", empId);
+        params.addValue("dateStart", start);
+        params.addValue("dateEnd", end);
+        params.addValue("transCodes", getTransCodesFromSet(types));
+
+        String sql = applyAuditColumnsInSelectSql(GET_TRANS_HISTORY_SQL, "audColumns", "", types);
+        List<TransactionRecord> transRecordList =
+                remoteNamedJdbc.query(sql, params, new TransactionRecordRowMapper("", "", types));
+
+        Map<TransactionType, TransactionHistory> transHistoryMap = new HashMap<>();
+        for (TransactionRecord tr : transRecordList) {
+            TransactionType type = tr.getTransType();
+            if (!transHistoryMap.containsKey(type)) {
+                transHistoryMap.put(type, new TransactionHistory(empId, type));
+            }
+            transHistoryMap.get(type).addTransactionRecord(tr);
+        }
+
+        return transHistoryMap;
+    }
+
+    /**
+     * Helper method to add audit columns to the select sql statement. This is done because the columns need to be
+     * explicitly added to prevent name clashes and we don't want to manually write them out.
+     * @param selectSql String - The sql with a select statement to add the audit columns to.
+     * @param replaceKey String - An identifier inside the sql for replacement. e.g ${auditCols} where 'auditCols' is the key.
+     *                            The replacement string cannot be the first entry in the select clause due to commas.
+     * @param pfx String - A prefix to apply to each column name. Leave empty if you just want the column name as is.
+     * @param restrictSet Set<TransactionType> - Only the columns for the desired types will be added. If null then
+     *                                           all the columns for every transaction type will be added.
+     * @return String - sql statement with audit columns
+     */
+    private String applyAuditColumnsInSelectSql(String selectSql, String replaceKey, String pfx, Set<TransactionType> restrictSet) {
+        Map<String, String> selectMap = new HashMap<>();
+
+        /** Restrict the columns to just the ones needed unless the set is empty or contains APP or RTP
+         *  because those serve as initial snapshots and therefore need all the columns. */
+        List<String> auditColList = new ArrayList<>();
+        if (restrictSet != null && !restrictSet.isEmpty() && !restrictSet.contains(TransactionType.APP) &&
+            !restrictSet.contains(TransactionType.RTP)) {
+            for (TransactionType type : restrictSet) {
+                auditColList.addAll(type.getDbColumnList());
+            }
+        }
+        else {
+            auditColList.addAll(TransactionType.getAllDbColumnsList());
+        }
+
+        /** Apply the prefix to the names if requested */
+        if (StringUtils.isNotEmpty(pfx)) {
+            for (int i = 0; i < auditColList.size(); i++) {
+                auditColList.set(i, pfx + auditColList.get(i));
+            }
+        }
+
+        /** Apply to the sql statement */
+        String auditColumns = StringUtils.join(auditColList, ",");
+        selectMap.put(replaceKey, (!auditColumns.isEmpty()) ? ("," + auditColumns) : "");
+        StrSubstitutor strSub = new StrSubstitutor(selectMap);
+        return strSub.replace(selectSql);
+    }
+
+    /**
+     * Helper method to return a set of the transaction codes in the 'types' set.
+     * @param types Set<TransactionType>
+     * @return Set<String>
+     */
+    private Set<String> getTransCodesFromSet(Set<TransactionType> types) {
+        Set<String> transCodes = new HashSet<>();
+        for (TransactionType type : types) {
+            transCodes.add(type.name());
+        }
+        return transCodes;
     }
 }
--- src/main/java/gov/nysenate/seta/model/TransactionHistory.java
+++ src/main/java/gov/nysenate/seta/model/TransactionHistory.java
@@ -2,25 +2,40 @@ package gov.nysenate.seta.model;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.TreeSet;
+import java.util.PriorityQueue;
 
+/**
+ * The TransactionHistory provides an ordered collection of TransactionRecords. This class is intended to be
+ * used in methods that need to know about the history of a specific TransactionType for an employee.
+ */
 public class TransactionHistory
 {
     protected int employeeId;
     protected TransactionType transType;
-    protected TreeSet<TransactionRecord> records;
+    protected PriorityQueue<TransactionRecord> records;
 
-    public TransactionHistory() {
-        this.records = new TreeSet<>(new Comparator<TransactionRecord>() {
+    public TransactionHistory(int empId, TransactionType type) {
+        this.employeeId = empId;
+        this.transType = type;
+        this.records = new PriorityQueue<>(11, new Comparator<TransactionRecord>() {
+            /** Order the records by most recent effect date/origin date */
             @Override
             public int compare(TransactionRecord o1, TransactionRecord o2) {
-                int dateCompare = o1.getEffectDate().compareTo(o2.getEffectDate());
-                return (dateCompare != 0) ? dateCompare : o1.getOriginalDate().compareTo(o2.originalDate);
+                int dateCompare = o2.getEffectDate().compareTo(o1.getEffectDate());
+                return (dateCompare != 0) ? dateCompare : o2.getOriginalDate().compareTo(o1.originalDate);
             }
         });
     }
 
+    public boolean hasRecords() {
+        return !records.isEmpty();
+    }
+
     /** Functional Getters/Setters */
+
+    public void addTransactionRecord(TransactionRecord record) {
+        this.records.add(record);
+    }
 
     public void addTransactionRecords(List<TransactionRecord> recordsList) {
         this.records.addAll(recordsList);
@@ -44,7 +59,7 @@ public class TransactionHistory
         this.transType = transType;
     }
 
-    public TreeSet<TransactionRecord> getRecords() {
+    public PriorityQueue<TransactionRecord> getRecords() {
         return this.records;
     }
 }
