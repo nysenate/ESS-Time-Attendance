e14ca9b3cb53ef15cca8b09dec81e7b5fcb50264
Ash Islam <ashislam858@gmail.com>
2014-04-02 11:46:15 -0400

    Added accrual rate helper

---
+++ src/main/java/gov/nysenate/seta/model/AccrualRate.java
@@ -0,0 +1,41 @@
+package gov.nysenate.seta.model;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Provides accrual rates for vacation and sick time based on number of biweekly pay periods.
+ */
+public enum AccrualRate
+{
+    /** Vacation rates increase as you work longer until you reach 5.5 */
+    VACATION (Arrays.asList(new BigDecimal(0), new BigDecimal("31.5"), new BigDecimal("3.5"),
+                            new BigDecimal("3.75"), new BigDecimal("4"), new BigDecimal("5.5"))),
+
+    /** Sick rates are fixed at 3.5 */
+    SICK     (Arrays.asList(new BigDecimal("3.5"), new BigDecimal("3.5"), new BigDecimal("3.5"),
+                            new BigDecimal("3.5"), new BigDecimal("3.5"), new BigDecimal("3.5")));
+
+    private ArrayList<BigDecimal> accRates;
+
+    AccrualRate(List<BigDecimal> accRates) {
+        assert accRates.size() == 6;
+        this.accRates = new ArrayList<>(accRates);
+    }
+
+    /**
+     * Retrieve the accrual rate based on the payPeriods.
+     * @param payPeriods int
+     * @return BigDecimal with rate stored
+     */
+    public BigDecimal getRate(int payPeriods) {
+        if (payPeriods <= 12) return accRates.get(0);
+        if (payPeriods == 13) return accRates.get(1);
+        if (payPeriods >= 14 && payPeriods <= 26) return accRates.get(2);
+        if (payPeriods >= 27 && payPeriods <= 52) return accRates.get(3);
+        if (payPeriods >= 53 && payPeriods <= 78) return accRates.get(4);
+        else return accRates.get(5);
+    }
+}
---
+++ test/main/java/gov/nysenate/seta/model/AccrualRateTests.java
@@ -0,0 +1,26 @@
+package gov.nysenate.seta.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AccrualRateTests
+{
+    @Test
+    public void testVacationRatesAreCorrect() throws Exception {
+        assertEquals("0", AccrualRate.VACATION.getRate(3).toString());
+        assertEquals("31.5", AccrualRate.VACATION.getRate(13).toString());
+        assertEquals("3.5", AccrualRate.VACATION.getRate(14).toString());
+        assertEquals("3.75", AccrualRate.VACATION.getRate(28).toString());
+        assertEquals("4", AccrualRate.VACATION.getRate(53).toString());
+        assertEquals("4", AccrualRate.VACATION.getRate(58).toString());
+        assertEquals("5.5", AccrualRate.VACATION.getRate(89).toString());
+    }
+
+    @Test
+    public void testSickRatesAreCorrect() throws Exception {
+        assertEquals("3.5", AccrualRate.SICK.getRate(3).toString());
+        assertEquals("3.5", AccrualRate.SICK.getRate(13).toString());
+        assertEquals("3.5", AccrualRate.SICK.getRate(900).toString());
+    }
+}
