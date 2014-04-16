02728fab5119a3df43b9d6210a41b3a90e6d0866
Ash Islam <ashislam858@gmail.com>
2014-04-16 16:26:50 -0400

    Removed stuff in AccrualService

---
+++ src/main/java/gov/nysenate/seta/dao/period/mapper/PayPeriodRowMapper.java
@@ -0,0 +1,28 @@
+package gov.nysenate.seta.dao.period.mapper;
+
+import gov.nysenate.seta.model.period.PayPeriod;
+import gov.nysenate.seta.model.period.PayPeriodType;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PayPeriodRowMapper implements RowMapper<PayPeriod>
+{
+    protected String pfx;
+
+    public PayPeriodRowMapper(String pfx) {
+        this.pfx = pfx;
+    }
+
+    @Override
+    public PayPeriod mapRow(ResultSet rs, int rowNum) throws SQLException {
+        PayPeriod period = new PayPeriod();
+        period.setActive(rs.getString("CDSTATUS").equals("A"));
+        period.setType(PayPeriodType.valueOf(rs.getString("CDPERIOD")));
+        period.setPayPeriodNum(rs.getInt("NUPERIOD"));
+        period.setStartDate(rs.getDate("DTBEGIN"));
+        period.setEndDate(rs.getDate("DTEND"));
+        return period;
+    }
+}
--- src/main/java/gov/nysenate/seta/service/AccrualService.java
+++ src/main/java/gov/nysenate/seta/service/AccrualService.java
@@ -1,41 +1,9 @@
 package gov.nysenate.seta.service;
 
-import gov.nysenate.seta.model.accrual.AccrualInfo;
-import gov.nysenate.seta.model.period.PayPeriod;
-import gov.nysenate.seta.model.personnel.Employee;
-
 /**
  * Service interface to provide accrual related functionality.
  */
 public interface AccrualService
 {
-    /**
-     * Retrieves the AccrualInfo for the latest pay period given an employee id.
-     * @param employeeId int
-     * @return AccrualInfo if employeeId is valid.
-     */
-    AccrualInfo getCurrentAccruals(int employeeId);
 
-    /**
-     * Retrieves the AccrualInfo for the latest pay period given an Employee.
-     * @param employee Employee
-     * @return AccrualInfo if Employee is valid.
-     */
-    AccrualInfo getCurrentAccruals(Employee employee);
-
-    /**
-     * Retrieves the AccrualInfo for the specified PayPeriod given the employee id.
-     * @param employeeId int
-     * @param payPeriod PayPeriod
-     * @return AccrualInfo if employee and pay period are valid.
-     */
-    AccrualInfo getAccrualsForPayPeriod(int employeeId, PayPeriod payPeriod);
-
-    /**
-     * Retrieves the AccrualInfo for the specified PayPeriod given the Employee.
-     * @param employee Employee
-     * @param payPeriod PayPeriod
-     * @return AccrualInfo if employee and pay period are valid.
-     */
-    AccrualInfo getAccrualsForPayPeriod(Employee employee, PayPeriod payPeriod);
 }
