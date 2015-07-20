8e0a52323fdd5bde8f64ac18ac491b654a8b01f4
Ash Islam <ashislam858@gmail.com>
2015-07-20 11:35:53 -0400

    Resolve merge conflict

--- src/test/java/gov/nysenate/seta/SillyTests.java
+++ src/test/java/gov/nysenate/seta/SillyTests.java
@@ -1,16 +1,6 @@
 package gov.nysenate.seta;
 
 import org.slf4j.Logger;
-
-<<<<<<< HEAD
-import com.google.common.collect.Range;
-import com.google.common.collect.RangeSet;
-import com.google.common.collect.Sets;
-import gov.nysenate.seta.model.transaction.TransactionCode;
-import org.junit.Test;
-=======
->>>>>>> ce73ba5decd2b2e7ef4d949d7646cf7b32785216
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
