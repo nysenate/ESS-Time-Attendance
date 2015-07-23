3df685a2ae6fc18c489034c6a285185b2782ec97
Ash Islam <ashislam858@gmail.com>
2015-07-23 12:00:01 -0400

    updated employees api

--- src/main/java/gov/nysenate/seta/controller/rest/EmployeeRestCtrl.java
+++ src/main/java/gov/nysenate/seta/controller/rest/EmployeeRestCtrl.java
@@ -1,23 +1,25 @@
 package gov.nysenate.seta.controller.rest;
 
 import gov.nysenate.seta.client.response.base.BaseResponse;
-import gov.nysenate.seta.client.response.base.SimpleResponse;
+import gov.nysenate.seta.client.response.base.ListViewResponse;
 import gov.nysenate.seta.client.response.base.ViewObjectResponse;
 import gov.nysenate.seta.client.view.DetailedEmployeeView;
 import gov.nysenate.seta.client.view.EmployeeView;
 import gov.nysenate.seta.dao.personnel.EmployeeDao;
+import gov.nysenate.seta.model.personnel.Employee;
 import gov.nysenate.seta.model.personnel.EmployeeException;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static gov.nysenate.seta.controller.rest.BaseRestCtrl.REST_PATH;
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequestMapping(REST_PATH + "/employees")
@@ -27,19 +29,21 @@ public class EmployeeRestCtrl extends BaseRestCtrl
 
     @Autowired protected EmployeeDao employeeDao;
 
-    @RequestMapping(value = "/empId/{empId}")
-    public BaseResponse getEmployeeById(@PathVariable Integer empId,
+    @RequestMapping(value = "")
+    public BaseResponse getEmployeeById(@RequestParam(required = true) Integer empId[],
                                         @RequestParam(defaultValue = "false") boolean detail) throws EmployeeException {
-        Subject subject = getSubject();
-//        subject.checkPermission("api:employees:view:" + empId);
-        return (detail) ? new ViewObjectResponse<>(new DetailedEmployeeView(employeeDao.getEmployeeById(empId)))
-                        : new ViewObjectResponse<>(new EmployeeView(employeeDao.getEmployeeById(empId)));
+        return getEmployeeResponse(
+            Arrays.asList(empId).stream().map(employeeDao::getEmployeeById).collect(toList()), detail);
     }
 
-    @RequestMapping("/email/{email}")
-    public BaseResponse getEmployeeByEmail(@PathVariable String email,
-                                           @RequestParam(defaultValue = "false") boolean detail) throws EmployeeException {
-        return (detail) ? new ViewObjectResponse<>(new DetailedEmployeeView(employeeDao.getEmployeeByEmail(email)))
-                        : new ViewObjectResponse<>(new EmployeeView(employeeDao.getEmployeeByEmail(email)));
+    private BaseResponse getEmployeeResponse(List<Employee> employeeList, boolean detail) {
+        if (employeeList.size() == 1) {
+            return new ViewObjectResponse<>((detail) ? new DetailedEmployeeView(employeeList.get(0))
+                                                     : new EmployeeView(employeeList.get(0)));
+        }
+        else {
+            return ListViewResponse.of(
+                employeeList.stream().map((detail) ? DetailedEmployeeView::new : EmployeeView::new).collect(toList()));
+        }
     }
 }
