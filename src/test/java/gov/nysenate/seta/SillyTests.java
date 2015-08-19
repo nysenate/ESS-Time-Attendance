eeb5b64fdae3f3c4ee2dbb75b93c9c5f03e22012
Ash Islam <ashislam858@gmail.com>
2015-08-19 16:08:00 -0400

    Cleanup sillytests

--- src/test/java/gov/nysenate/seta/SillyTests.java
+++ src/test/java/gov/nysenate/seta/SillyTests.java
@@ -1,13 +1,12 @@
 package gov.nysenate.seta;
 
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * A sample file to run misc tests.
  */
-public class SillyTests extends BaseTests
+public class SillyTests  extends BaseTests
 {
     private static final Logger logger = LoggerFactory.getLogger(SillyTests.class);
 }
