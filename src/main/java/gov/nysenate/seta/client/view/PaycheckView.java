c777e660d1db65c8dd1ad42b8b7fb06e0efdddc5
Kevin Caseiras <kcaseiras@gmail.com>
2015-10-07 12:10:58 -0400

    Paycheck views

---
+++ src/main/java/gov/nysenate/seta/client/view/PaycheckView.java
@@ -0,0 +1,110 @@
+package gov.nysenate.seta.client.view;
+
+import gov.nysenate.seta.client.view.base.ViewObject;
+import gov.nysenate.seta.model.payroll.Paycheck;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@XmlRootElement
+public class PaycheckView implements ViewObject
+{
+    protected String empFullName;
+    protected String empJobTitle;
+    protected BigDecimal payRate;
+    protected String payPeriod;
+    protected LocalDate checkDate;
+    protected String agencyCode;
+    protected String lineNum;
+    protected BigDecimal grossIncome;
+    protected BigDecimal netIncome;
+    protected List<DeductionView> deductions;
+    protected BigDecimal directDepositAmount;
+    protected BigDecimal checkAmount;
+
+    public PaycheckView(Paycheck paycheck) {
+        this.empFullName = paycheck.getEmpFullName();
+        this.empJobTitle = paycheck.getEmpJobTitle();
+        this.payRate = paycheck.getPayRate();
+        this.payPeriod = paycheck.getPayPeriod();
+        this.checkDate = paycheck.getCheckDate();
+        this.agencyCode = paycheck.getAgencyCode();
+        this.lineNum = paycheck.getLineNum();
+        this.grossIncome = paycheck.getGrossIncome();
+        this.netIncome = paycheck.getNetIncome();
+        this.deductions = paycheck.getDeductions().stream().map(DeductionView::new).collect(toList());
+        this.directDepositAmount = paycheck.getDirectDepositAmount();
+        this.checkAmount = paycheck.getCheckAmount();
+    }
+
+    @Override
+    @XmlElement
+    public String getViewType() {
+        return "Paycheck";
+    }
+
+    @XmlElement
+    public String getEmpFullName() {
+        return empFullName;
+    }
+
+    @XmlElement
+    public String getEmpJobTitle() {
+        return empJobTitle;
+    }
+
+    @XmlElement
+    public BigDecimal getPayRate() {
+        return payRate;
+    }
+
+    @XmlElement
+    public String getPayPeriod() {
+        return payPeriod;
+    }
+
+    @XmlElement
+    public LocalDate getCheckDate() {
+        return checkDate;
+    }
+
+    @XmlElement
+    public String getAgencyCode() {
+        return agencyCode;
+    }
+
+    @XmlElement
+    public String getLineNum() {
+        return lineNum;
+    }
+
+    @XmlElement
+    public BigDecimal getGrossIncome() {
+        return grossIncome;
+    }
+
+    @XmlElement
+    public BigDecimal getNetIncome() {
+        return netIncome;
+    }
+
+    @XmlElement
+    public List<DeductionView> getDeductions() {
+        return deductions;
+    }
+
+    @XmlElement
+    public BigDecimal getDirectDepositAmount() {
+        return directDepositAmount;
+    }
+
+    @XmlElement
+    public BigDecimal getCheckAmount() {
+        return checkAmount;
+    }
+}
